b18e616e023299a49ebdf5d9cad285a79b0c77dc
vickymmcd <vickymmcd@gmail.com>
2019-10-31 20:25:09 -0400

    getting socket working

--- backend/hydroshare_gui/__init__.py
+++ backend/hydroshare_gui/__init__.py
@@ -1,3 +1,8 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 1025
+
 def _jupyter_server_extension_paths():
     return [{
         "module": "hydroshare_gui"
@@ -6,3 +11,16 @@ def _jupyter_server_extension_paths():
 
 def load_jupyter_server_extension(nbapp):
     nbapp.log.info("CUAHSI module enabled!")
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print("listening...")
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
