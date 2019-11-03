73a1b81714738138e155013615fbdb270da497fa
charlievweiss <charlievweiss@gmail.com>
2019-11-03 16:08:39 -0500

    updating

--- backend/hydroshare_gui/__init__.py
+++ backend/hydroshare_gui/__init__.py
@@ -1,4 +1,5 @@
 import socket
+from get_info import get_metadata
 
 HOST = '127.0.0.1'
 PORT = 1025
@@ -20,7 +21,10 @@ def load_jupyter_server_extension(nbapp):
         with conn:
             print('Connected by', addr)
             while True:
-                data = conn.recv(1024)
+                # data = conn.recv(1024)
+                test_resource_id = '8b826c43f55043f583c85ae312a8894f'
+                data = get_metadata(test_resource_id)['resource_title']
                 if not data:
                     break
                 conn.sendall(data)
+                break
