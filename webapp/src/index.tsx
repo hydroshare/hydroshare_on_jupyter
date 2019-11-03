f5a146ad92a3a67dc250dc580c7d221f47ef7b2b
Emily Lepert <emily.lepert@students.olin.edu>
2019-11-02 22:39:00 -0500

    Finally got a working app with store and reducer
    file structure changed a bunch didn't want to mess up the structure we currently have

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,21 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/App.scss';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import setupStore from './redux/setup-store';
-import * as serviceWorker from './serviceWorker';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
-const store = setupStore();
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root') as HTMLElement
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
