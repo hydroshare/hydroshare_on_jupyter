984aab425208cdd1e861ee3d6519ed9ea34eb2c8
Kyle Combes <kylecombes@gmail.com>
2019-11-08 13:14:41 -0500

    React Router connected to Redux but Routes not working properly.

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -3,14 +3,35 @@ import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './styles/App.css';
 
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import store from './store';
+import store, { history } from './store';
+
+import Header from './components/Header';
+import FilterBar from './components/FilterBar';
+import ResourceList from './components/ResourceList';
+import FileList from './components/FileList';
+import {ConnectedRouter} from 'connected-react-router';
+import {Route} from 'react-router';
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      {/*<Switch>*/}
+        <Route path="/">
+          <Header />
+        </Route>
+        <Route path="/">
+        <FilterBar />
+        </Route>
+        <Route exact={true} path="/">
+          <ResourceList />
+        </Route>
+        <Route path="/">
+          <FileList />
+        </Route>
+      {/*</Switch>*/}
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
--- webapp/src/store/index.ts
+++ webapp/src/store/index.ts
@@ -1,4 +1,15 @@
-import { combineReducers, createStore } from 'redux';
+import {
+  connectRouter, routerMiddleware,
+} from 'connected-react-router';
+import {
+  createBrowserHistory,
+} from 'history';
+import {
+  applyMiddleware,
+  compose,
+  combineReducers,
+  createStore,
+} from 'redux';
 
 import {
   filterReducer,
@@ -10,12 +21,22 @@ import {
   IRootState,
 } from './types';
 
+export const history = createBrowserHistory();
+
 const store = createStore<IRootState, any, any, any>(
     combineReducers({
         filter: filterReducer,
         mainPage: mainPageReducer,
         projects: projectsReducer,
+        router: connectRouter(history),
         user: userReducer,
-    }));
+    }),
+    {},
+    compose(
+      applyMiddleware(
+        routerMiddleware(history),
+      ),
+    ),
+  );
 
 export default store;
