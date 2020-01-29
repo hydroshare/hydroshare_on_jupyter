cc4681dfd66b43089f32d4d2295ea7087dc5d0b3
charlievweiss <charlievweiss@gmail.com>
2020-01-29 13:29:14 -0500

    deleting old files and adding documentation

--- backend/new_file.py
+++
@@ -1,7 +0,0 @@
-import hs_restclient
-from login import username, password
-
-a = hs_restclient.HydroShareAuthBasic(username=username,
-                                      password=password)
-hs = hs_restclient.HydroShare(auth=a)
-hs.resources()
