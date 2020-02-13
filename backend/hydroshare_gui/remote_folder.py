bf67b190cc5b9763c241fe36c6a2b228a9e427d8
Emily Lepert <emily.lepert@students.olin.edu>
2020-02-13 16:32:40 -0500

    Delete files from JH and HS working

--- backend/hydroshare_gui/remote_folder.py
+++ backend/hydroshare_gui/remote_folder.py
@@ -7,12 +7,22 @@ Email: vickymmcd@gmail.com
 '''
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from hs_restclient import HydroShare, HydroShareAuthBasic
+from login import username, password
 
 
 
 ''' Class that defines a Remote Folder so we can access attributes of it.
 '''
 class RemoteFolder:
+
+    def __init__(self):
+        '''Sets up authentication on hydroshare API.
+        '''
+        # authentication for using Hydroshare API
+        auth = HydroShareAuthBasic(username=username, password=password)
+        self.hs = HydroShare(auth=auth)
+
     def get_file_metadata(self, filepath, size):
         """Gets file definition formatting for returning HS files, given path
         & size. Returns false if the path is a folder & not a file.
@@ -54,3 +64,7 @@ class RemoteFolder:
                 folder_size += nested_files[v[2]]["size"]
 
         return folder_size, contents
+
+    def delete_file(self, res_id, filepath):
+        resource_id = self.hs.deleteResourceFile(res_id, filepath)
+
