a4a6e49cbd916360d1f9a49e13f96e612d685708
Kyle Combes <kylecombes@gmail.com>
2020-03-03 22:17:46 -0500

    File syncing using drag and drop (#9)
    * Resolve source and destination IFile and/or IFolder on item drop. Split IFileOrFolder into IFile and IFolder.
    
    * Drag and drop logs source and destination to console. Waiting on HS paths from backend.
    
    * Try moving file from JH to HS. Fixed CORS error, but KeyError in backend now.
    
    * Fix API call to move file from JH to HS.
    
    * Rename file transfer endpoint to /resources/:resid/file-ops. Update API documentation.
    
    * Send file move request when dragging and dropping within HydroShare or JupyterHub filesystems.
    
    * Remove erroneous lines.
    
    * Fix "NaNUndefiend" sizes in FileManager.
    
    * Fix "Last Modified" just displaying current date in FileManager.
    
    * Fix backend exceptions after downloading a resource with no files.
    
    * Fix moving files within JH or HS.
    
    * Add comment on backend response headers.
    
    * Fix resource files not loaded when refreshing ResourcePage.
    
    * Remove unneeded try...except.
    
    * Fix import formatting.

--- backend/hydroshare_gui/hs_server.py
+++ backend/hydroshare_gui/hs_server.py
@@ -12,53 +12,45 @@ import signal
 import logging
 import sys
 import json
-from resource2 import Resource
+from resource2 import Resource, HS_PREFIX, JH_PREFIX
 from resource_handler import ResourceHandler
 
 import tornado.ioloop
 import tornado.web
 import tornado.options
 
-
 # Global resource handler variable
 resource_handler = ResourceHandler()
 
-''' Function that configures cors for a handler to allow our server to access it
-'''
-def configure_cors(handler):
-    handler.set_header("Access-Control-Allow-Origin", "*") # TODO: change from * (any server) to our specific url
-    handler.set_header("Access-Control-Allow-Headers", "x-requested-with")
-    handler.set_header('Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE, OPTIONS')
 
-
-''' Class that handles starting up the frontend for our web app
-'''
-class WebAppHandler(tornado.web.RequestHandler):
-
+class BaseRequestHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
-        configure_cors(self)
+        self.set_header("Access-Control-Allow-Origin", "*")  # TODO: change from * (any server) to our specific url
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with, content-type")
+        # TODO: Do this on a per-handler basis (not all of them allow all of these requests)
+        self.set_header('Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE, OPTIONS')
+
+    def options(self, _):
+        self.set_status(204)  # No content
+        self.finish()
+
+
+class WebAppHandler(BaseRequestHandler):
+    """ Handles starting up the frontend for our web app """
 
     def get(self):
         self.render('index.html')
 
 
-'''
-'''
-class BundleHandler(tornado.web.RequestHandler):
-    def set_default_headers(self):
-        configure_cors(self)
-
+class BundleHandler(BaseRequestHandler):
+    """ Serves the web app JavaScript file """
     def get(self):
         self.render('bundle_link')
 
 
-''' Class that handles GETing a list of a user's resources (with metadata) & POSTing
-a new resource for that user
-'''
-class ResourcesHandler(tornado.web.RequestHandler):
-
-    def set_default_headers(self):
-        configure_cors(self)
+class ResourcesHandler(BaseRequestHandler):
+    """ Class that handles GETing a list of a user's resources (with metadata) & POSTing
+     a new resource for that user """
 
     def get(self):
         # TODO: Probably do some request error handling here
@@ -95,17 +87,14 @@ class ResourcesHandler(tornado.web.RequestHandler):
         else:
             self.write("Please specify title and creators to make new resource")
 
-''' Class that handles DELETEing file in JH
-'''
-class FileHandlerJH(tornado.web.RequestHandler):
 
-    def set_default_headers(self):
-        configure_cors(self)
+class FileHandlerJH(BaseRequestHandler):
+    """ Class that handles DELETEing file in JH """
 
     def get(self, res_id):
         resource = Resource(res_id, resource_handler)
         jh_files = resource.get_files_JH()
-        self.write({'files': jh_files})
+        self.write({'rootDir': jh_files})
 
     def delete(self, res_id):
         body = json.loads(self.request.body.decode('utf-8'))
@@ -116,7 +105,7 @@ class FileHandlerJH(tornado.web.RequestHandler):
         else:
             self.write("Please specify filepath to delete")
 
-    def put(self,res_id):
+    def put(self, res_id):
         body = json.loads(self.request.body.decode('utf-8'))
         resource = Resource(res_id, resource_handler)
         request_type = body.get("request_type")
@@ -142,19 +131,14 @@ class FileHandlerJH(tornado.web.RequestHandler):
                     'JH_files': jh_files})
 
 
-''' Class that handles GETing list of a files that are in a user's
-hydroshare instance of a resource
-'''
-class FileHandlerHS(tornado.web.RequestHandler):
-
-    def set_default_headers(self):
-        configure_cors(self)
+class FileHandlerHS(BaseRequestHandler):
+    """ Class that handles GETing list of a files that are in a user's HydroShare instance of a resource """
 
     def get(self, res_id):
         # TODO: Get folder info
         resource = Resource(res_id, resource_handler)
-        hs_files = resource.get_files_HS()
-        self.write({'files': hs_files})
+        root_dir = resource.get_files_HS()
+        self.write({'rootDir': root_dir})
 
     def delete(self, res_id):
         body = json.loads(self.request.body.decode('utf-8'))
@@ -163,42 +147,104 @@ class FileHandlerHS(tornado.web.RequestHandler):
             resource = Resource(res_id, resource_handler)
             resource.delete_file_or_folder_from_HS(filepath)
             resource.update_hs_files()
-            self.write({"files": resource.hs_files})
+            self.write({"rootDir": resource.hs_files})
         else:
             self.write("Please specify filepath to delete")
 
-    def put(self,res_id):
-        body = json.loads(self.request.body.decode('utf-8'))
-        resource = Resource(res_id, resource_handler)
-        request_type = body.get("request_type")
-        if request_type == "rename_or_move_file":
-            resource.rename_file_HS(body.get("old_filepath"), body.get("new_filepath"))
-        elif request_type == "overwrite_JH":
-            resource.overwrite_JH_with_file_from_HS(body.get("filepath"))
-        else:
-            self.write("Please specify valid request type for PUT")
-            return
 
-        resource.update_hs_files()
-        self.write({"HS_files": resource.hs_files})
+MOVE = 'move'
+COPY = 'copy'
 
 
-''' Class that handles GETing user information on the currently logged
-in user including name, email, username, etc.
-'''
-class UserInfoHandler(tornado.web.RequestHandler):
+class FileOperationHandler(BaseRequestHandler):
+    """ Handles moving (or renaming) files within the local filesystem, on HydroShare, and between the two. """
 
     def set_default_headers(self):
-        configure_cors(self)
+        BaseRequestHandler.set_default_headers(self)
+        self.set_header('Access-Control-Allow-Methods', 'PATCH, OPTIONS')
+
+    def patch(self, res_id):
+        body = json.loads(self.request.body.decode('utf-8'))
+        resource = Resource(res_id, resource_handler)
+        file_operations = body['operations']
+
+        results = []
+        success_count = 0
+        failure_count = 0
+
+        for operation in file_operations:
+            method = operation['method']  # 'copy' or 'move'
+            src_uri = operation['source']
+            dest_uri = operation['destination']
+            do_force = operation.get('force', False)
+
+            # Split paths into filesystem prefix ('hs' or 'local') and path relative to the resource root on
+            # that filesystem
+            src_fs, src_path = src_uri.split(':')
+            dest_fs, dest_path = dest_uri.split(':')
+
+            # Remove the leading forward slashes
+            src_path = src_path[1:]
+            dest_path = dest_path[1:]
+
+            if src_fs == HS_PREFIX and dest_fs == HS_PREFIX:
+                if method == MOVE:  # Move or rename
+                    # TODO: Test how well this works
+                    resource.rename_or_move_file_HS(src_path, dest_path)
+                    results.append({'success': True})
+                    success_count += 1
+                else:  # TODO: Copy
+                    raise NotImplementedError('Copy within HydroShare not implemented')
+            elif src_fs == JH_PREFIX and dest_fs == JH_PREFIX:
+                # TODO: Move/rename/copy file on local filesystem
+                if method == MOVE:  # Move or rename
+                    resource.rename_or_move_file_JH(src_path, dest_path)
+                    results.append({'success': True})
+                    success_count += 1
+                else:  # Copy
+                    raise NotImplementedError('Copy within the local filesystem not implemented yet')
+            elif src_fs == JH_PREFIX and dest_fs == HS_PREFIX:
+                # Transfer the file regardless of if we're moving or copying
+                # TODO: Support moving from one local folder to a different one on HS
+                resource.overwrite_HS_with_file_from_JH(src_path)
+                if method == MOVE:
+                    # TODO: Delete the local copy of the file
+                    pass
+                results.append({'success': True})
+                success_count += 1
+            elif src_fs == HS_PREFIX and dest_fs == JH_PREFIX:
+                # TODO: Move/copy file from HS to the local filesystem
+                raise NotImplementedError()
+            else:
+                msg = f'"source" prefix "{src_fs}" and/or destination prefix "{dest_fs} not recognized. Valid options' \
+                      f' are "hs" and "local"'
+                logging.warning(msg)
+                results.append({
+                    'success': False,
+                    'error': 'UnrecognizedPathPrefix',
+                    'message': msg,
+                })
+                failure_count += 1
+
+        self.write({
+            'results': results,
+            'successCount': success_count,
+            'failureCount': failure_count,
+        })
+
+
+class UserInfoHandler(BaseRequestHandler):
+    """ Class that handles GETing user information on the currently logged
+    in user including name, email, username, etc. """
 
     def get(self):
         data = resource_handler.get_user_info()
         self.write(data)
 
 
-''' Class for setting up the server & making sure it can exit cleanly
-'''
 class HydroShareGUI(tornado.web.Application):
+    """ Class for setting up the server & making sure it can exit cleanly """
+
     is_closing = False
 
     def signal_handler(self, signum, frame):
@@ -219,24 +265,26 @@ def make_app():
         (r"/user", UserInfoHandler),
         (r"/resources", ResourcesHandler),
         (r"/resources/([^/]+)/hs-files", FileHandlerHS),
-        (r"/resources/([^/]+)/local-files", FileHandlerJH)
+        (r"/resources/([^/]+)/local-files", FileHandlerJH),
+        (r"/resources/([^/]+)/file-ops", FileOperationHandler),
     ])
 
-''' Starts running the server
-'''
-def start_server(application):
+
+def start_server(app):
+    """ Starts running the server """
     tornado.options.parse_command_line()
-    signal.signal(signal.SIGINT, application.signal_handler)
-    application.listen(8080)
-    tornado.ioloop.PeriodicCallback(application.try_exit, 100).start()
+    signal.signal(signal.SIGINT, app.signal_handler)
+    app.listen(8080)
+    tornado.ioloop.PeriodicCallback(app.try_exit, 100).start()
     tornado.ioloop.IOLoop.instance().start()
+
 
 if __name__ == '__main__':
     LEVELS = {'debug': logging.DEBUG,
-          'info': logging.INFO,
-          'warning': logging.WARNING,
-          'error': logging.ERROR,
-          'critical': logging.CRITICAL}
+              'info': logging.INFO,
+              'warning': logging.WARNING,
+              'error': logging.ERROR,
+              'critical': logging.CRITICAL}
 
     if len(sys.argv) > 1:
         level_name = sys.argv[1]
--- webapp/src/store/actions/resources.ts
+++ webapp/src/store/actions/resources.ts
@@ -1,25 +1,59 @@
 import { action } from 'typesafe-actions';
+import {
+  ThunkAction,
+  ThunkDispatch,
+} from "redux-thunk";
+import { AnyAction } from "redux";
+
+import { ResourcesActions } from './action-names';
+import {
+  getResourceHydroShareFiles,
+  getResourceLocalFiles,
+} from "../async-actions";
 
 import {
-  IFileOrFolder,
+  IFolder,
   IJupyterResource,
+  IRootState,
 } from '../types';
-import { ResourcesActions } from './action-names';
+
+export function getFilesIfNeeded(resource: IJupyterResource): ThunkAction<Promise<void>, {}, {}, AnyAction> {
+    return async (dispatch: ThunkDispatch<{}, {}, AnyAction>, getState: () => IRootState) => {
+        const {
+          resourceLocalFilesBeingFetched,
+          resourceHydroShareFilesBeingFetched,
+        } = getState().resources;
+        if (resource && !resource.jupyterHubFiles && !resourceLocalFilesBeingFetched.has(resource.id)) {
+            dispatch(getResourceLocalFiles(resource));
+        }
+        if (resource && resource.hydroShareResource && !resource.hydroShareResource.files && !resourceHydroShareFilesBeingFetched.has(resource.id)) {
+            dispatch(getResourceHydroShareFiles(resource));
+        }
+    };
+}
+
+export function notifyGettingResourceHydroShareFiles(resource: IJupyterResource) {
+  return action(ResourcesActions.NOTIFY_GETTING_RESOURCE_HYDROSHARE_FILES, { resourceId: resource.id });
+}
+
+export function notifyGettingResourceJupyterHubFiles(resource: IJupyterResource) {
+  return action(ResourcesActions.NOTIFY_GETTING_RESOURCE_JUPYTERHUB_FILES, { resourceId: resource.id });
+}
 
 export function setResources(resources: IJupyterResource[]) {
   return action(ResourcesActions.SET_RESOURCES, resources);
 }
 
-export function setResourceLocalFiles(resourceId: string, files: IFileOrFolder[]) {
+export function setResourceLocalFiles(resourceId: string, rootDir: IFolder) {
   return action(ResourcesActions.SET_RESOURCE_LOCAL_FILES, {
     resourceId,
-    files,
+    rootDir,
   });
 }
 
-export function setResourceHydroShareFiles(resourceId: string, files: IFileOrFolder[]) {
+export function setResourceHydroShareFiles(resourceId: string, rootDir: IFolder) {
   return action(ResourcesActions.SET_RESOURCE_HYDROSHARE_FILES, {
     resourceId,
-    files,
+    rootDir,
   });
 }
