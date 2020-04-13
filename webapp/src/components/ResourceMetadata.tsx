e168b182f5c62595fcd2f143678591a193953500
Emily <elepert@olin.edu>
2020-04-12 17:04:56 -0700

    Finished resource metaadata section

--- webapp/src/components/ResourceMetadata.tsx
+++ webapp/src/components/ResourceMetadata.tsx
@@ -7,6 +7,7 @@ import '../styles/ResourceMetadata.scss';
 
 export interface IPropTypes {
   resource: IJupyterResource
+  promptEditPrivacy: () => any
 }
 
 export default class ResourceMetadata extends React.Component<IPropTypes, never> {
@@ -20,17 +21,36 @@ export default class ResourceMetadata extends React.Component<IPropTypes, never>
         <div className="resource-info">
             <div className="info-wrapping">
                 <div className="info-group">
-                    <span className="info-header">Author</span>
+                    <span className="info-header">Creator</span>
                     <p>{hydroShareResource.creator}</p>
+                </div>
+                <div className="info-group">
+                    <span className="info-header">Authors</span>
+                    <p>{hydroShareResource.authors}</p>
+                </div>
+                <div className="info-group">
+                    <span className="info-header">Resource Type</span>
+                    <p>{hydroShareResource.resource_type}</p>
+                </div>
+                <div className="info-group">
+                    <span className="info-header">Created</span>
+                    <p>{hydroShareResource.date_created}</p>
                 </div>
                 <div className="info-group">
                     <span className="info-header">Last Modified</span>
                     <p>{hydroShareResource.date_last_updated.format('MMM D, YYYY')}</p>
                 </div>
+                <div className="info-group">
+                    <span className="info-header">Sharing Status</span>
+                    <div>
+                      <p className="info-content">{hydroShareResource.public ? "Public" : "Private"}</p>
+                      <p className ="info-edit" onClick={this.props.promptEditPrivacy}>edit</p>
+                    </div>
+                </div>
             </div>
             <div className="info-group">
                 <span className="info-header">Abstract</span>
-                <p>{hydroShareResource.abstract}</p>
+                <p>{hydroShareResource.abstract ? hydroShareResource.abstract : "No abstract yet"}</p>
             </div>
         </div>
     ) : null;
