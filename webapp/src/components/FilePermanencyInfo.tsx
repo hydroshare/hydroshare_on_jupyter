fe139f57deef1222fe1367aa259526991194a08d
Emily <elepert@olin.edu>
2020-04-14 21:31:52 -0700

    Created first draft of archive info section

--- webapp/src/components/FilePermanencyInfo.tsx
+++ webapp/src/components/FilePermanencyInfo.tsx
@@ -8,19 +8,23 @@ export default class FilePermanencyInfo extends React.Component<any, never> {
     const hsResourceMeta =
       <div className="permanency-info">
         <div className="info-title">
-            <h2>How permanent are your files?</h2>
+            <h2>When should you archive this project?</h2>
         </div>
         <div className="info-wrapping">
-          <div className="info-group">
+          <div className="info-group selected">
             <img className="info-img" src="/assets/CUAHSI-logo.png" alt="CUAHSI logo"></img>
             <p className="info-header">CUAHSI JupyterHub</p>
+            <p className="info-content">Archive your project when you're done working on it</p>
           </div>
           <div className="info-group">
             <img className="info-img" src="/assets/Binder-logo.png" alt="Binder logo"></img>
             <p className="info-header">Binder JupyterHub</p>
+            <p className="info-content">Archive your project before you close your Binder instance</p>
           </div>
           <div className="info-group">
-            <span className="info-header">Resource Type</span>
+            <img className="info-img" src="/assets/Desktop-logo.png" alt="Binder logo"></img>
+            <p className="info-header">Local JupyterHub</p>
+            <p className="info-content">Archive your project whenever you want</p>
           </div>
         </div>
       </div>
