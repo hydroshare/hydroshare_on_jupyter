36e873699d1d828dc073b0a33bb64a9f174ffb3e
soumyapurohit <soumya13p@gmail.com>
2020-07-08 21:11:11 -0600

    Added preferences in header

--- webapp/src/components/Header.tsx
+++ webapp/src/components/Header.tsx
@@ -11,7 +11,9 @@ import {
 import {
   ThunkDispatch,
 } from "redux-thunk";
-
+import {
+  logoutToHydroShare
+} from '../store/async-actions';
 // @ts-ignore
 const ASSETS_URL = (window.FRONTEND_URL || '') + '/assets';
 
@@ -22,9 +24,20 @@ const mapStateToProps = ({ user }: IRootState) => {
 const mapDispatchToProps = (dispatch: ThunkDispatch<{}, {}, any>) => ({
   goHome: () => dispatch(goHome()),
   loadInitData: () => dispatch(loadInitData()),
+  logout: () => dispatch(logoutToHydroShare())
 });
 
 type ReduxType = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
+const elementPreferences = {
+  options: [
+
+   {id : 1, value : 'Preferences'},
+   {id : 2, value : 'Change directory'},
+   {id : 3, value : 'Update Config'},
+   {id : 4, value : 'logout'}
+ ] ,
+ value : ''
+ }
 
 /**
  * Compnent that contains the logo and the return to all my resources button
@@ -35,6 +48,24 @@ class Header extends React.Component<ReduxType, never> {
   public componentDidMount(): void {
     // TODO: Move this somewhere better
     this.props.loadInitData();
+  }
+
+  onClicking(event: React.ChangeEvent<HTMLSelectElement>) {
+    console.log(event);
+     const e = event.target.value;
+     console.log(e);
+     if (e === '1') {
+      this.props.logout()
+     }
+     else if (e == '2') {
+       console.log('yet to be defined')
+     }
+     else if (e == '3'){
+      console.log('wait, we are getting close!')
+    }
+    else {
+     this.props.logout()
+    }
   }
 
   public render() {
@@ -50,6 +81,9 @@ class Header extends React.Component<ReduxType, never> {
             <button className="go-home clickable" onClick={this.props.goHome}>
               Back to all my Resources
             </button>
+            <select onChange={(event) => this.onClicking(event)}>{elementPreferences.options.map(option => (
+              <option key = {option.id} value={option.id}>{option.value}</option>
+              ))} </select>
           </div>
         </div>
       </header>
