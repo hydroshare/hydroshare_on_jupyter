820c9580bfe0ee608ce47f50b1d762732ee038b5
Austin Raney <araney@cuahsi.org>
2021-07-14 15:45:29 -0400

    add session struct container for holding session and session cookie

---
+++ hydroshare_jupyter_sync/session_struct.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from hs_restclient import HydroShare
+
+# type hint imports
+from typing import Union
+
+
+@dataclass
+class SessionStruct:
+    session: Union[HydroShare, None]
+    cookie: Union[bytes, None]
+
+    def __eq__(self, o: Union[bytes, "SessionStruct"]) -> bool:
+        """Check if self.cookie is equal to o. If self.cookie is none, False."""
+        if self.cookie == None:
+            return False
+        if isinstance(o, SessionStruct):
+            return self.cookie == o.cookie
+        if not isinstance(o, bytes):
+            return False
+        return self.cookie == o
