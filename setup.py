916db4058ffa32b8348bd819027582d2200a0c76
Austin Raney <araney@cuahsi.org>
2021-07-15 15:36:37 -0400

    add pytest-tornado to development requirements

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ REQUIREMENTS = [
 ]
 
 # Development requirements
-DEVELOPMENT_REQUIREMENTS = ["pytest"]
+DEVELOPMENT_REQUIREMENTS = ["pytest", "pytest-tornado"]
 
 SHORT_DESCRIPTION = "A web app and server for syncing files between the local filesystem and HydroShare. Can run as a Jupyter notebook extension."
 
