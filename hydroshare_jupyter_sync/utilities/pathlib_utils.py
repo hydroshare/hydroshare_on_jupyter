8099b18ef131a11c98f56a8d2d2277892faeb885
Austin Raney <araney@cuahsi.org>
2021-07-16 16:59:22 -0400

    pathlib utilities for verifying if a path is a descendant of some parent

--- hydroshare_jupyter_sync/utilities/pathlib_utils.py
+++ hydroshare_jupyter_sync/utilities/pathlib_utils.py
@@ -17,3 +17,39 @@ def expand_and_resolve(p: Union[str, Path]) -> Path:
     Path
     """
     return Path(p).expanduser().resolve()
+
+
+def expand_and_resolve_path_to_posix(p: Union[str, Path]) -> str:
+    """Given a string or Path, expand the user (~) and resolve the absolute path and
+    return it's POSIX string representation.
+
+    Parameters
+    ----------
+    p : Union[str, Path]
+        string or path
+
+    Returns
+    -------
+    str
+        POSIX absolute path
+    """
+    return expand_and_resolve(p).as_posix()
+
+
+def is_descendant(child: Union[str, Path], parent: Union[str, Path]) -> bool:
+    """Determine if a child path is descendant of some parent path.
+
+    Parameters
+    ----------
+    child : Union[str, Path]
+    parent : Union[str, Path]
+
+    Returns
+    -------
+    bool
+        Is child of parent?
+    """
+    child = expand_and_resolve_path_to_posix(child)
+    parent = expand_and_resolve_path_to_posix(parent)
+
+    return child.startswith(parent)
