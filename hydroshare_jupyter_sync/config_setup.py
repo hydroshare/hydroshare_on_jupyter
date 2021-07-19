6fcdddb1e82554b55f519067e913b3f84200f6b4
Austin Raney <araney@cuahsi.org>
2021-07-19 10:26:53 -0400

    add configuration file parser that uses pydantic.
    Current configuration parameters are `data` and `log`. Both are
    directories. By default, hydroshare_juypter_sync looks for them at
    ~/.config/hydroshare_jupyter_sync/config and
    ~/.hydroshare_jupyter_sync_config in order. Meaning, if
    ~/.config/hydroshare_jupyter_sync/config is found first,
    ~/.hydroshare_jupyter_sync_config will not be read.
    
    Data and log parameters can be specified in multiple ways.
    1. Config files mentioned above using shell environment variable like
       semantics (i.e. DATA=/some/path).
    2. As environment variables (i.e. export DATA=/some/path)
    3. Using the CLI by passing a non-default configuration file
       (i.e. --config env_file).

---
+++ hydroshare_jupyter_sync/config_setup.py
@@ -0,0 +1,38 @@
+from pydantic import BaseSettings, root_validator
+from pathlib import Path
+from typing import Union
+from .utilities.pathlib_utils import first_existing_file, expand_and_resolve
+
+_DEFAULT_CONFIG_FILE_LOCATIONS = (
+    "~/.config/hydroshare_jupyter_sync/config",
+    "~/.hydroshare_jupyter_sync_config",
+)
+
+_DEFAULT_DATA_PATH = expand_and_resolve("~/hydroshare")
+_DEFAULT_LOG_PATH = expand_and_resolve("~/hydroshare/logs")
+
+
+class FileNotDirectoryError(Exception):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+
+class ConfigFile(BaseSettings):
+    data: Path = _DEFAULT_DATA_PATH
+    log: Path = _DEFAULT_LOG_PATH
+
+    class Config:
+        env_file: Union[str, None] = first_existing_file(_DEFAULT_CONFIG_FILE_LOCATIONS)
+        env_file_encoding = "utf-8"
+
+    @root_validator
+    def create_paths_if_do_not_exist(cls, values: dict):
+        for key, path in values.items():
+            if path.is_file():
+                raise FileNotDirectoryError(
+                    f"Configuration setting: {key}={str(path)} is a file not a directory."
+                )
+            elif not path.exists():
+                path.mkdir()
+
+        return values
