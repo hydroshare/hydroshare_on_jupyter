5e0be5921ad9ac8b29b360c4cca246502ebdfe16
Austin Raney <araney@cuahsi.org>
2021-09-27 10:48:08 -0400

    update webpack configuration. @lumino, @jupyterlab, and react treated as peer dependencies, meaning, they are expected to exist in the application environment (i.e. juypter).

--- webapp/config/webpack.config.js
+++ webapp/config/webpack.config.js
@@ -1,43 +1,42 @@
 const path = require('path');
 const fs = require('fs');
 
+// measure webpack speed https://github.com/stephencookdev/speed-measure-webpack-plugin
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const { exit } = require('process');
+const smp = new SpeedMeasurePlugin();
+
 const APP_DIR = path.dirname(__dirname); // webapp
 const BUILD_DIR = path.resolve(APP_DIR, 'build');
 
-module.exports = {
-  entry: path.join(APP_DIR, 'src', 'index.tsx'),
+module.exports = smp.wrap({
+  entry: path.join(APP_DIR, 'src', 'index.ts'),
   output: {
     path: BUILD_DIR,
     publicPath: '/',
-    filename: 'bundle.js',
+    // filename: 'bundle.js',
+    filename: 'index.js',
+    libraryTarget: "umd"
   },
   mode: 'production',
   module: {
     rules: [
       {
         test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        use: ["ts-loader"],
+        // exclude: /node_modules/,
+        use: [ "ts-loader" ],
+        // use: [{ loader: "ts-loader", options: { allowTsInNodeModules: true }} ],
       },
       {
         test: /\.jsx?/,
         include: APP_DIR,
-        exclude: /node_modules/,
+        // exclude: /node_modules/,
         use: ['babel-loader'],
       },
       {
         test: /\.svg$/,
-        use: [
-          {
-            loader: "babel-loader",
-          },
-          {
-            loader: "react-svg-loader",
-            options: {
-              jsx: true, // true outputs JSX tags
-            }
-          }
-        ]
+        // use: ["@svgr/webpack"]
+        use:["raw-loader"]
       },
       {
         test: /\.scss$/,
@@ -53,5 +52,21 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    // extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+    // modules: ["node_modules", "node_modules/chonky/dist"],
+    // alias: {
+    //   "chonky/dist" : "node_modules/chonky/dist"
+    // },
+    fallback: {
+      // required for chonky
+      "path": require.resolve("path-browserify")
+    }
   },
-};
+  externals: [
+    // exclude peer dependencies from bundle
+    // may want to move this to it's own webpack config that is specific for the jupyter widget
+    /^@lumino\/.+$/,
+    /^@jupyterlab\/.+$/,
+    /^react$/
+  ]
+});
