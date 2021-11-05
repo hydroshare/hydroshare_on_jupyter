8b0b755f761f5f5e36264f6d71e0da707a1f1ec4
Austin Raney <araney@cuahsi.org>
2021-11-05 09:51:14 -0400

    add AuthState, ResourceId, FSState, and ResourceState types

--- webapp/src/store/sync-api/types.ts
+++ webapp/src/store/sync-api/types.ts
@@ -5,3 +5,21 @@ type ResourceFilePrefix = "/data/contents/";
 //   F extends string
 // > = `${P}${F}`;
 export type TResourceFile = string;
+
+export type AuthState = {
+  status: boolean;
+};
+
+export type ResourceId = string;
+
+export type FSState = {
+  resource_id: ResourceId;
+  only_local: string[];
+  only_remote: string[];
+  out_of_sync: string[];
+  in_sync: string[];
+};
+
+export type ResourceState = {
+  [Property in keyof ResourceId]?: FSState;
+};
