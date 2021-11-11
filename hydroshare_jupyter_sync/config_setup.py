00878c964b39803400fe35ed683631facaba58be
Austin Raney <araney@cuahsi.org>
2021-11-11 15:41:44 -0500

    add OAUTH field to configuration. specify the location of an oauth file (~/.hs_auth).

--- hydroshare_jupyter_sync/config_setup.py
+++ hydroshare_jupyter_sync/config_setup.py
@@ -1,7 +1,9 @@
-from pydantic import BaseSettings, Field, root_validator
+from pydantic import BaseSettings, Field, root_validator, validator
+import pickle
 from pathlib import Path
-from typing import Union
+from typing import Optional, Union
 from .utilities.pathlib_utils import first_existing_file, expand_and_resolve
+from .models.oauth import OAuthFile
 
 _DEFAULT_CONFIG_FILE_LOCATIONS = (
     "~/.config/hydroshare_jupyter_sync/config",
@@ -21,20 +23,33 @@ class ConfigFile(BaseSettings):
     # case-insensitive alias values DATA and LOG
     data_path: Path = Field(_DEFAULT_DATA_PATH, env="data")
     log_path: Path = Field(_DEFAULT_LOG_PATH, env="log")
+    oauth_path: Optional[OAuthFile] = Field(None, env="oauth")
 
     class Config:
         env_file: Union[str, None] = first_existing_file(_DEFAULT_CONFIG_FILE_LOCATIONS)
         env_file_encoding = "utf-8"
 
-    @root_validator
-    def create_paths_if_do_not_exist(cls, values: dict):
-        for key, path in values.items():
-            path = expand_and_resolve(path)
-            if path.is_file():
-                raise FileNotDirectoryError(
-                    f"Configuration setting: {key}={str(path)} is a file not a directory."
-                )
-            elif not path.exists():
-                path.mkdir()
+    @validator("data_path", "log_path")
+    def create_paths_if_do_not_exist(cls, v: Path):
+        # for key, path in values.items():
+        path = expand_and_resolve(v)
+        if path.is_file():
+            raise FileNotDirectoryError(
+                f"Configuration path: {str(path)} is a file not a directory."
+            )
+        elif not path.exists():
+            path.mkdir()
+        return v
 
-        return values
+    @validator("oauth_path", pre=True)
+    def unpickle_oauth_path(cls, v):
+        if v is None:
+            return
+        path = expand_and_resolve(v)
+        if not path.is_file():
+            error_message = "Provided OAUTH configuration value must be file."
+            raise FileNotFoundError(error_message)
+
+        with open(path, "rb") as f:
+            deserialized = pickle.load(f)                
+        return deserialized
