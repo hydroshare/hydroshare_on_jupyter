05ebc05d89cbcf37cca2be85e65b1e8662ae589c
Austin Raney <araney@cuahsi.org>
2021-11-11 15:32:16 -0500

    update __init__.py to follow new jupyter_server serverapp conventions

--- hydroshare_jupyter_sync/__init__.py
+++ hydroshare_jupyter_sync/__init__.py
@@ -3,6 +3,8 @@ Setup jupyterlab server and lab extensions. Add tornado HTTP handlers to jupyter
 """
 import json
 from pathlib import Path
+from jupyter_server.serverapp import ServerApp
+from jupyter_server.utils import url_path_join
 
 # local imports
 from .config_setup import ConfigFile
@@ -12,7 +14,6 @@ from .handlers import get_route_handlers
 FRONTEND_PATH = "/sync"
 BACKEND_PATH = "/syncApi"
 
-MODULE_NAME = "hydroshare_jupyter_sync"
 EXTENSION_DIRNAME = "labextension"
 
 PARENT_DIR = Path(__file__).parent.resolve()
@@ -20,17 +21,26 @@ EXTENSION_METADATA_PATH = PARENT_DIR / f"{EXTENSION_DIRNAME}/package.json"
 
 # read metadata from js extension package metadata file, `package.json`
 extension_metadata = json.loads(EXTENSION_METADATA_PATH.read_text())
+MODULE_NAME = extension_metadata["name"]
 
 
 def _jupyter_labextension_paths():
-    return [{"src": EXTENSION_DIRNAME, "dest": extension_metadata["name"]}]
+    return [{"src": EXTENSION_DIRNAME, "dest": MODULE_NAME}]
 
 
-def _jupyter_server_extension_paths():
+# def _jupyter_server_extension_paths():
+#     return [{"module": MODULE_NAME}]
+
+
+def _jupyter_server_extension_points():
+    """
+    Returns a list of dictionaries with metadata describing
+    where to find the `_load_jupyter_server_extension` function.
+    """
     return [{"module": MODULE_NAME}]
 
 
-def _load_jupyter_server_extension(server_app):
+def _load_jupyter_server_extension(server_app: ServerApp):
     """Registers the API handler to receive HTTP requests from the frontend extension.
 
     Parameters
@@ -38,7 +48,11 @@ def _load_jupyter_server_extension(server_app):
     server_app: jupyterlab.labapp.LabApp
         JupyterLab application instance
     """
-    handlers = get_route_handlers(FRONTEND_PATH, BACKEND_PATH)
+    web_app = server_app.web_app
+    base_url = web_app.settings["base_url"]
+    handlers = get_route_handlers(
+        url_path_join(base_url, FRONTEND_PATH), url_path_join(base_url, BACKEND_PATH)
+    )
 
     # `cookie_secret` inherited from `server_app`
     server_app.web_app.add_handlers(".*$", handlers)
