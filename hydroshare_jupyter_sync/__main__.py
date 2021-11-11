8c450e399d9beb36c22bfe8fd7f673bd5b00ff6e
Austin Raney <araney@cuahsi.org>
2021-11-11 15:33:36 -0500

    add helper methods to main for linking server and labextension to the correct jupyter path locations. used by cli

--- hydroshare_jupyter_sync/__main__.py
+++ hydroshare_jupyter_sync/__main__.py
@@ -1,16 +1,27 @@
 import argparse
 import logging
+import os
 import signal
 import sys
+from pathlib import Path
+
+from jupyter_core.paths import ENV_JUPYTER_PATH
+from jupyter_server.extension.serverextension import EnableServerExtensionApp
 
 from tornado import options
 from tornado import ioloop
 from tornado.web import Application
 from pathlib import Path
 from .handlers import get_route_handlers
-from .cli import parse
+from .cli import CommandNamespace, parse
 from .config_setup import ConfigFile
 
+MODULE_DIR = Path(__file__).parent.resolve()
+_ERROR_MESSAGE_PREFIX = "Fatal, cannot link labextension."
+
+# path to pre-built lab-extension
+EXTENSION_NAME = "hydroshare_jupyter_sync"
+LABEXTENSION_PATH = MODULE_DIR / "labextension"
 
 class TestApp(Application):
     """Class for setting up the server & making sure it can exit cleanly"""
@@ -40,33 +51,111 @@ def get_test_app(**settings) -> Application:
     )
 
 
-def main(parser: argparse.Namespace):
+def enable_server_extension(extension_name: str):
+    o = EnableServerExtensionApp()
+    o.toggle_server_extension(extension_name)
+
+
+def get_env_jupyter_path() -> Path:
+    """Absolute path to Jupyter's ENV_JUPYTER_PATH.
+    If multiple ENV_JUPYTER_PATH's are specified, the first is returned.
+    """
+    try:
+        return Path(ENV_JUPYTER_PATH[0]).resolve()
+    except IndexError as e:
+        error_message = f"{_ERROR_MESSAGE_PREFIX} ENV_CONFIG_PATH not set."
+        raise ValueError(error_message) from e
+
+
+def link_prebuilt_labextension(
+    labextension_name: str, labextension_prebuilt_files_dir: str
+):
+    labextension_prebuilt_files_dir = Path(labextension_prebuilt_files_dir).resolve()
+
+    # naively verify that labextension exists (does not verify contents of directory. i.e. package.json exists)
+    if (
+        not labextension_prebuilt_files_dir.exists()
+        or not labextension_prebuilt_files_dir.is_dir()
+    ):
+        error_message = f"{_ERROR_MESSAGE_PREFIX} labextension_prebuilt_files_dir: {labextension_prebuilt_files_dir} does not exist."
+        raise FileNotFoundError(error_message)
+
+    labextensions_path = get_env_jupyter_path() / "labextensions"
+
+    # create `labextensions` directory if it does not already exist. Create intermediate dirs if necessary
+    labextensions_path.mkdir(exist_ok=True, parents=True)
+
+    link_dir = labextensions_path / labextension_name
+
+    # choose not to fail raise exception if link_dir exists.
+    if link_dir.exists():
+        if link_dir.is_symlink() and os.readlink(str(link_dir)) == str(
+            labextension_prebuilt_files_dir
+        ):
+            print(f"{labextension_prebuilt_files_dir} already linked to {link_dir}")
+        else:
+            print(
+                f"Could not link {labextension_prebuilt_files_dir} to {link_dir}. {link_dir} already exists. Remove {link_dir} and reinstall."
+            )
+    else:
+        # note: target_is_directory True, required for windows support
+        link_dir.symlink_to(labextension_prebuilt_files_dir, target_is_directory=True)
+
+        print(f"{labextension_prebuilt_files_dir} linked to {link_dir}")
+
+
+def configure_jupyter() -> None:
+    # link lab extension to correct location and enable server extension
+    link_prebuilt_labextension(EXTENSION_NAME, LABEXTENSION_PATH)
+    enable_server_extension(EXTENSION_NAME)
+
+
+def start_stand_alone_session(
+    hostname: str, port: int, debug: bool, config: ConfigFile
+) -> None:
+    app = get_test_app(default_hostname=hostname, debug=debug, **config.dict())
+
+    logging.info(f"Server starting on {hostname}:{port}")
+    logging.info(f"Debugging mode {'enabled' if debug else 'disabled'}")
+
+    signal.signal(signal.SIGINT, app.signal_handler)
+    app.listen(port)
+    ioloop.PeriodicCallback(app.try_exit, 100).start()
+    ioloop.IOLoop.instance().start()
+
+
+def main(parser: argparse.Namespace) -> None:
     # clear argv. options parse command line somehow sets up logging
     # tornados logging setup is pretty broken. Do not want to pass any command line args
     # here
     sys.argv = []
     options.parse_command_line()
-    debug_enabled = not parser.no_debug
-    # TODO: write logs to file in config.log
 
-    # parse config file
-    config = (
-        ConfigFile() if parser.config is None else ConfigFile(_env_file=parser.config)
-    )
+    # command routing
+    if parser.command == CommandNamespace.configure:
+        configure_jupyter()
 
-    app = get_test_app(
-        default_hostname=parser.hostname, debug=debug_enabled, **config.dict()
-    )
+    elif parser.command == CommandNamespace.start:
+        # TODO: write logs to file in config.log
+        # parse config file
+        config = (
+            ConfigFile()
+            if parser.config is None
+            else ConfigFile(_env_file=parser.config)
+        )
 
-    logging.info(f"Server starting on {parser.hostname}:{parser.port}")
-    logging.info(f"Debugging mode {'enabled' if debug_enabled else 'disabled'}")
+        kwargs = {
+            "hostname": parser.hostname,
+            "port": parser.port,
+            "debug": not parser.debug,
+            "config": config,
+        }
 
-    signal.signal(signal.SIGINT, app.signal_handler)
-    app.listen(parser.port)
-    ioloop.PeriodicCallback(app.try_exit, 100).start()
-    ioloop.IOLoop.instance().start()
+        start_stand_alone_session(**kwargs)
 
 
 if __name__ == "__main__":
     parser = parse()
-    main(parser)
+    if len(sys.argv) <= 1:
+        parser.print_help()
+    main(parser.parse_args())
