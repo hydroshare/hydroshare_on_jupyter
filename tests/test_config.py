ab024b53b116309bfd0f09f8ebe97713129db653
Austin Raney <araney@cuahsi.org>
2021-11-12 10:33:54 -0500

    add configuration unit tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,7 @@
 import pytest
+import pickle
 from hydroshare_jupyter_sync.config_setup import ConfigFile, FileNotDirectoryError
-from tempfile import TemporaryDirectory
+from tempfile import TemporaryDirectory, TemporaryFile
 from pathlib import Path
 
 
@@ -44,3 +45,33 @@ def test_config_using_env_file():
         c = ConfigFile(_env_file=env_file)
         assert str(c.data_path) == str(temp)
         assert str(c.log_path) == str(log)
+
+
+@pytest.fixture
+def oauth_data():
+    return [
+        {
+            "access_token": "some_fake_token",
+            "token_type": "Bearer",
+            "refresh_token": "some_fake_token",
+            "scope": "scope",
+            "expires_in": 2592000,
+        },
+        "some_fake_token",
+    ]
+
+
+@pytest.fixture
+def oauth_file(oauth_data) -> Path:
+
+    filename = ".hs_auth"
+    with TemporaryDirectory() as dir:
+        oauth_path = Path(dir) / filename
+        with open(oauth_path, "wb") as f:
+            pickle.dump(oauth_data, f, protocol=2)
+        yield oauth_path
+
+
+def test_config_oauth(oauth_file, oauth_data):
+    o = ConfigFile(oauth_path=oauth_file)
+    assert o.oauth_path.access_token == oauth_data[0]["access_token"]
