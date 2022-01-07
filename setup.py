3d4ad7c671f9a68c6d4ff2c18d22e842f0c6c2e6
Austin Raney <araney@cuahsi.org>
2022-01-07 15:15:01 -0500

    bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 # define package name globally
 PKGNAME = "hydroshare_on_jupyter"
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 
 # define webapp path globally
 here = Path(__file__).resolve().parent
